3f7440b3ddd57146441ae5d4b2b156b9574a13f0
Replit user <replituser@example.com>
2021-03-15 02:33:37 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -108,6 +108,7 @@ Programe la función int obtenerValor(Vector * v, int i),
 la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) {
+  printf("%d",v->datos[i]);
    return 0;
 }
 
